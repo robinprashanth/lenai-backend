f886f7608bd46d3d6495212b9134fbc181b71bb4
robin <robin.prashantha@gmail.com>
2023-06-12 23:40:42 +0530

    testingd

--- ingestdata.py
+++ ingestdata.py
@@ -7,7 +7,6 @@ from langchain.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from util import move_files
-ENV_VALUES = dotenv_values('.env')
 #
 
 embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPEN_API_KEY"))
@@ -17,13 +16,6 @@ pinecone.init(
         )
 pineConeIndex = pinecone.Index(index_name=os.getenv("PINECONE_INDEX"))
 index = Pinecone.from_existing_index(index_name=os.getenv("PINECONE_INDEX"), embedding=embeddings)
-
-# Get new files and modified files list
-new_files_list = sys.argv[1].split(",")
-modified_list = sys.argv[2].split(",")
-
-print(f"There are {len(new_files_list)} new files found")
-print(f"There are {len(modified_list)} modified files found")
 
 def addNewTextDoc(path):
     loader = TextLoader(path, encoding="utf-8")
@@ -45,35 +37,16 @@ def split_docs(documents,chunk_size=1000, chunk_overlap=20):
     print (f"Now you have {len(docs)} documents that have an average of {num_total_characters / len(docs):,.0f} characters (smaller pieces)")
     return docs
 
-# if len(new_files_list) > 0:
-#     for file in new_files_list:
-#         split_tup = os.path.splitext(file)
-#         fileExtention = split_tup[1]    
-
-#         if fileExtention == ".txt":
-#             print("***"*20)
-#             print(f"Converting the {file} into documents")
-#             addNewTextDoc(file)
-#             print(f"Successfully converted {file} file into embedded vectors and added to the pinecone database")
-#             print("***"*20)
-#         if fileExtention == ".pdf":
-#             print("***"*20)
-#             print(f"Converting the {file} into documents")
-#             addNewPDFDoc(file)
-#             print(f"Successfully converted {file} file into embedded vectors and added to the pinecone database")
-#             print("***"*20)
-#     print("***Ingestion Complete***")#
-# else:
-#     print("No new files found in the data folder. Skipping pinecone vector db operations.")
-
 def load_docs(documentspath):
        loader = DirectoryLoader(documentspath)
        documents = loader.load()
        print (f"You have {len(documents)} document")
        return documents
 
-if len(new_files_list) > 0:
+def ingets_data():
     documents = load_docs("newData")
     docs = split_docs(documents)
     Pinecone.from_documents(docs, embeddings, index_name=os.getenv("PINECONE_INDEX"))
     move_files("newdata", "data")
+
+ingets_data()
